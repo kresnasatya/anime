6c3e9f84efbacff74be693f226914a758a1c98ac
Kresna Satya <kresnasatya72@gmail.com>
2024-08-10 18:23:47 +0800

    setup style little bit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import anime from '$lib/anime.json';
+</script>
+
+<div style="text-align: center;">
+    <h1>anime.kresna.me</h1>
+    <p><em>List of Anime that Kresna has watched. Made with SvelteKit.</em></p>
+</div>
+
+<ul>
+    {#each anime as item}
+        <li><a href="/anime/{item.slug}"><span>{item.name}</span><span>{item.released_year}</span></a></li>
+    {/each}
+</ul>
+
+<style>
+    ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1rem;
+    }
+
+    li > a {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-decoration: none;
+        color: black;
+        font-weight: 400;
+    }
+</style>
