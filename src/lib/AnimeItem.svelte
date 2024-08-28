3ee75e9e7824f4af531eece1a90d6c944bc2581b
Kresna Satya <kresnasatya72@gmail.com>
2024-08-28 15:08:16 +0800

    create AnimeItem component

---
+++ src/lib/AnimeItem.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let anime;
+    export let href;
+
+	import AnimeCard from "./AnimeCard.svelte";
+</script>
+
+<li><a href={href}><AnimeCard anime={anime}/></a></li>
+
+<style>
+    li > a {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-decoration: none;
+        color: black;
+        font-weight: 400;
+    }
+</style>
