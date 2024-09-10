103bbe468c19d3b19f337b9a63e48f0223cb3d01
Kresna Satya <satyakresna@unud.ac.id>
2024-09-10 16:16:08 +0800

    add anime and fix typo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -46,7 +46,7 @@
 <div style="text-align: center;">
     <h1><a href="/">anime.kresna.me</a></h1>
     <p>List of Anime that I have watched.</p>
-    <p>I have watched {anime.length} anime series and stil counting. How about you?</p>
+    <p>I have watched {anime.length} anime series and still counting. How about you?</p>
     <p><em>Let's nostalgia together! 🤘🤘</em></p>
 </div>
 
